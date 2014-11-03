2a52e454fe91c12986aa5a13babb57579e6bbb4e
joohyoung Yang <yangjoo@debian.yangjoo.net>
2014-11-03 23:09:44 +0900

    remove advanced reboot checkbox option for use default
    Change-Id: I6b2cdd8da53c61bab9d6d35a5e2ef7f4ad08616c

--- services/java/com/android/server/power/ShutdownThread.java
+++ services/java/com/android/server/power/ShutdownThread.java
@@ -154,11 +154,10 @@ public final class ShutdownThread extends Thread {
                 boolean isPrimary = UserHandle.getCallingUserId() == UserHandle.USER_OWNER;
 
                 // See if the advanced reboot menu is enabled (only if primary user) and check the keyguard state
-                boolean advancedReboot = isPrimary ? advancedRebootEnabled(context) : false;
                 KeyguardManager km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
                 boolean locked = km.inKeyguardRestrictedInputMode() && km.isKeyguardSecure();
 
-                if (advancedReboot && !locked) {
+                if (!locked) {
                     // Include options in power menu for rebooting into recovery or bootloader
                     sConfirmDialog = new AlertDialog.Builder(context)
                             .setTitle(titleResourceId)
@@ -219,10 +218,6 @@ public final class ShutdownThread extends Thread {
         } else {
             beginShutdownSequence(context);
         }
-    }
-
-    private static boolean advancedRebootEnabled(Context context) {
-        return Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.ADVANCED_REBOOT, 0) == 1;
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver
