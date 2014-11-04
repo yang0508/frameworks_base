d0b91ec40da282176ad05aee58b48d77eda4846b
joohyoung Yang <yangjoo@debian.yangjoo.net>
2014-11-04 23:10:31 +0900

    remove advanced reboot enable option for use default (1/2)

--- services/java/com/android/server/power/ShutdownThread.java
+++ services/java/com/android/server/power/ShutdownThread.java
@@ -168,15 +168,10 @@ public final class ShutdownThread extends Thread {
                 sConfirmDialog = null;
             }
             if (mReboot && !mRebootSafeMode) {
-                // Determine if primary user is logged in
-                boolean isPrimary = UserHandle.getCallingUserId() == UserHandle.USER_OWNER;
-
-                // See if the advanced reboot menu is enabled (only if primary user) and check the keyguard state
-                boolean advancedReboot = isPrimary ? advancedRebootEnabled(context) : false;
                 KeyguardManager km = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
                 boolean locked = km.inKeyguardRestrictedInputMode() && km.isKeyguardSecure();
 
-                if (advancedReboot && !locked) {
+                if (!locked) {
                     // Include options in power menu for rebooting into recovery or bootloader
                     sConfirmDialog = new AlertDialog.Builder(context)
                             .setTitle(titleResourceId)
@@ -254,10 +249,6 @@ public final class ShutdownThread extends Thread {
         } else {
             beginShutdownSequence(context);
         }
-    }
-
-    private static boolean advancedRebootEnabled(Context context) {
-        return Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.ADVANCED_REBOOT, 0) == 1;
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver
