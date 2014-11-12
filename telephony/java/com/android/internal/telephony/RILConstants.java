19e0c5cb4015f1bd0fc5d6ae4e648275f249f101
joohyoung Yang <yangjoo@debian.yangjoo.net>
2014-11-12 09:35:50 +0900

    default preferred network type set for SHW-M440S
    Change-Id: Ic54325f614d518806e89e1e4c264fd201e0a2ac8

--- telephony/java/com/android/internal/telephony/RILConstants.java
+++ telephony/java/com/android/internal/telephony/RILConstants.java
@@ -99,7 +99,7 @@ public interface RILConstants {
     int NETWORK_MODE_TD_SCDMA_CDMA_EVDO_GSM_WCDMA  = 21; /*TD-SCDMA,EvDo,CDMA,GSM/WCDMA*/
     int NETWORK_MODE_TD_SCDMA_LTE_CDMA_EVDO_GSM_WCDMA = 22; /* TD-SCDMA/LTE/GSM/WCDMA, CDMA, and
                                                                EvDo */
-    int PREFERRED_NETWORK_MODE      = NETWORK_MODE_LTE_GSM_WCDMA;
+    int PREFERRED_NETWORK_MODE      = NETWORK_MODE_WCDMA_PREF;
 
     int CDMA_CELL_BROADCAST_SMS_DISABLED = 1;
     int CDMA_CELL_BROADCAST_SMS_ENABLED  = 0;
